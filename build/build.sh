761d2c1e4eaa5e9be1bb46f7414a8d8a1fe85d6f
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-10-22 10:46:46 -0400

    Revert heading de-increment
    Based on discussion at https://github.com/manubot/rootstock/issues/273,
    I have decided that the Pandoc convention of using h1 for sections
    as well as the title is bizarre. It makes most sense for us to stick
    with the Manubot convention of h2 for top-level sections like abstract.
    This means numbered sections will always be problematic in HTML
    output because they will be like 0.2.4. Therefore, revert --number-sections.

--- build/build.sh
+++ build/build.sh
@@ -39,8 +39,6 @@ pandoc --verbose \
   --bibliography="$BIBLIOGRAPHY_PATH" \
   --csl="$CSL_PATH" \
   --metadata link-citations=true \
-  --number-sections \
-  --base-header-level=2 \
   --include-after-body=build/themes/default.html \
   --include-after-body=build/plugins/table-scroll.html \
   --include-after-body=build/plugins/anchors.html \
